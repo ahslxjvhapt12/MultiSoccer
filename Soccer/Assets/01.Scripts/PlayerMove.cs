c281d12d85b002fdd0a2936bc30fa1d82e561c7d
ahslxjvhapt12 <likjhee1@gmail.com>
2023-09-21 20:17:28 +0900

    Kick

--- Soccer/Assets/01.Scripts/PlayerMove.cs
+++ Soccer/Assets/01.Scripts/PlayerMove.cs
@@ -1,7 +1,10 @@
+using DG.Tweening;
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class PlayerMove : MonoBehaviour
 {
@@ -9,8 +12,11 @@ public class PlayerMove : MonoBehaviour
     [SerializeField][Range(1f, 15f)] private float speed = 5f;
     [SerializeField][Range(0f, 1f)] private float rayDinstance = 1f;
     [SerializeField][Range(1f, 10f)] private float jumpPower = 10f;
+    [SerializeField][Range(0f, 1f)] private float kickDuration = 0.3f;
+    [SerializeField][Range(0f, 1f)] private float kickIntervalTime = 0.1f;
     [SerializeField] private float maxHP = 100;
     private float currentHP;
+    private bool onKick = false;
 
     [Header("-")]
     [SerializeField] private Transform origin = null;
@@ -19,45 +25,86 @@ public class PlayerMove : MonoBehaviour
 
     private Rigidbody2D _rigid;
 
+    private event Action EndOfKick;
+
     private void Awake()
     {
         _rigid = GetComponent<Rigidbody2D>();
         currentHP = maxHP;
     }
 
+    private void OnEnable()
+    {
+        EndOfKick += SetAnchorScale;
+    }
+
     private void Update()
     {
         Move();
-        Jump();
-        Kick();
-        Dash();
+        if (Input.GetKeyDown(KeyCode.Space))
+            Jump();
+        if (Input.GetMouseButtonDown(0) && onKick == false)
+            Kick();
+        if (Input.GetMouseButtonDown(1))
+            Dash();
+    }
+
+    private void OnDisable()
+    {
+        EndOfKick -= SetAnchorScale;
     }
 
     private void Move()
     {
+        Debug.Log("Move호출");
         float h = Input.GetAxis("Horizontal");
         float x = Input.GetAxisRaw("Horizontal");
         transform.position += new Vector3(h, 0, 0) * Time.deltaTime * speed;
         if (x != 0)
-            visual.transform.localScale = new Vector3(-x, 1, 1);
-    }
-    private void Jump()
-    {
-        if (Input.GetKeyDown(KeyCode.Space))
         {
-            if (Physics2D.Raycast(origin.position, Vector2.down, rayDinstance))
-            {
-                _rigid.AddForce(transform.up * jumpPower, ForceMode2D.Impulse);
-            }
+            visual.transform.localScale = new Vector3(-x, 1, 1);
+            if (onKick == false)
+                SetAnchorScale();
         }
     }
+
+    private void Jump()
+    {
+        Debug.Log("Jump호출");
+        if (Physics2D.Raycast(origin.position, Vector2.down, rayDinstance))
+        {
+            _rigid.AddForce(transform.up * jumpPower, ForceMode2D.Impulse);
+        }
+    }
+
     private void Kick()
     {
-
+        Debug.Log("Kick호출");
+        DG.Tweening.Sequence seq = DOTween.Sequence()
+            .PrependCallback(() =>
+            {
+                onKick = true;
+                footAnchor.SetActive(true);
+            })
+            .Append(footAnchor.transform.DORotate(new Vector3(0, 0, footAnchor.transform.localScale.x * -90), kickDuration))
+            .AppendInterval(0.1f)
+            .OnComplete(() =>
+            {
+                footAnchor.SetActive(false);
+                footAnchor.transform.rotation = new Quaternion(0, 0, 0, 0);
+                onKick = false;
+                EndOfKick?.Invoke();
+            });
     }
+
+    private void SetAnchorScale()
+    {
+        footAnchor.transform.localScale = visual.transform.localScale;
+    }
+
     private void Dash()
     {
-
+        Debug.Log("Dash호출");
     }
 
     private void OnDrawGizmosSelected()
